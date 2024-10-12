634e00eca1db091bfe0cd3fa2cf4aa77e9fca7e6
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-10-12 22:52:36 +0100

    reset to last commit, replace all occurences of home with dashboard

--- resources/js/Pages/Landing.tsx
+++ resources/js/Pages/Landing.tsx
@@ -34,7 +34,7 @@ export default function Landing({
                                     href={route('dashboard')}
                                     className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                 >
-                                    <Button>Dashboard</Button>
+                                    <Button className={"rounded-full"}>Dashboard</Button>
                                 </Link>
                                 <ModeToggle />
                             </div>
