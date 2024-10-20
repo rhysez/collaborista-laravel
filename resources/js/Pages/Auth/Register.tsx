92beac488a3150955361c3581dd27055930e3134
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-10-20 12:21:48 +0100

    add platform selection to register route

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -1,18 +1,24 @@
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
-import { FormEventHandler } from 'react';
+import {Head, Link, useForm} from '@inertiajs/react';
+import {FormEventHandler, useState} from 'react';
+import {Button} from "@/Components/ui/button";
+import {ToggleGroup, ToggleGroupItem} from "@/Components/ui/toggle-group"
+
 
 export default function Register() {
-    const { data, setData, post, processing, errors, reset } = useForm({
+    type FormContent = "Account" | "Platform" | "Personal";
+    const [content, setContent] = useState<FormContent>("Account");
+
+    const {data, setData, post, processing, errors, reset} = useForm({
         name: '',
         username: '',
         email: '',
         password: '',
         password_confirmation: '',
+        platforms: [],
     });
 
     const submit: FormEventHandler = (e) => {
@@ -22,6 +28,8 @@ export default function Register() {
             onFinish: () => reset('password', 'password_confirmation'),
         });
     };
+
+    console.log(data);
 
     return (
         <GuestLayout>
@@ -33,111 +41,160 @@ export default function Register() {
             </section>
 
             <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name"/>
+                {
+                content == "Account" ?
+                <>
+                    <div>
+                        <InputLabel htmlFor="name" value="Name (can be first name, or full name)"/>
 
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
-                        required
-                    />
+                        <TextInput
+                            id="name"
+                            name="name"
+                            value={data.name}
+                            className="mt-1 block w-full"
+                            autoComplete="name"
+                            onChange={(e) => setData('name', e.target.value)}
+                            required
+                        />
 
-                    <InputError message={errors.name} className="mt-2"/>
-                </div>
+                        <InputError message={errors.name} className="mt-2"/>
+                    </div>
 
-                <div className={"mt-4"}>
-                    <InputLabel htmlFor="username" value="Username"/>
+                    <div className={"mt-4"}>
+                        <InputLabel htmlFor="username" value="Username"/>
 
-                    <TextInput
-                        id="username"
-                        name="username"
-                        value={data.username}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData('username', e.target.value)}
-                        required
-                    />
+                        <TextInput
+                            id="username"
+                            name="username"
+                            value={data.username}
+                            className="mt-1 block w-full"
+                            autoComplete="username"
+                            onChange={(e) => setData('username', e.target.value)}
+                            required
+                        />
 
-                    <InputError message={errors.username} className="mt-2"/>
-                </div>
+                        <InputError message={errors.username} className="mt-2"/>
+                    </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email"/>
+                    <div className="mt-4">
+                        <InputLabel htmlFor="email" value="Email"/>
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
-                    />
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="mt-1 block w-full"
+                            autoComplete="email"
+                            onChange={(e) => setData('email', e.target.value)}
+                            required
+                        />
 
-                    <InputError message={errors.email} className="mt-2"/>
-                </div>
+                        <InputError message={errors.email} className="mt-2"/>
+                    </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password"/>
+                    <div className="mt-4">
+                        <InputLabel htmlFor="password" value="Password"/>
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                        required
-                    />
+                        <TextInput
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            className="mt-1 block w-full"
+                            autoComplete="new-password"
+                            onChange={(e) => setData('password', e.target.value)}
+                            required
+                        />
 
-                    <InputError message={errors.password} className="mt-2"/>
-                </div>
+                        <InputError message={errors.password} className="mt-2"/>
+                    </div>
 
-                <div className="mt-4">
-                    <InputLabel
-                        htmlFor="password_confirmation"
-                        value="Confirm Password"
-                    />
+                    <div className="mt-4">
+                        <InputLabel
+                            htmlFor="password_confirmation"
+                            value="Confirm Password"
+                        />
 
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) =>
-                            setData('password_confirmation', e.target.value)
-                        }
-                        required
-                    />
+                        <TextInput
+                            id="password_confirmation"
+                            type="password"
+                            name="password_confirmation"
+                            value={data.password_confirmation}
+                            className="mt-1 block w-full"
+                            autoComplete="new-password"
+                            onChange={(e) =>
+                                setData('password_confirmation', e.target.value)
+                            }
+                            required
+                        />
 
-                    <InputError
-                        message={errors.password_confirmation}
-                        className="mt-2"
-                    />
-                </div>
+                        <InputError
+                            message={errors.password_confirmation}
+                            className="mt-2"
+                        />
+                    </div>
 
-                <div className="mt-4 flex items-center justify-end">
-                    <Link
-                        href={route('login')}
-                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+                    <div className="mt-4 flex items-center justify-end">
+                        <Link
+                            href={route('login')}
+                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+                        >
+                            Already registered?
+                        </Link>
+
+                        <Button onClick={() => setContent("Platform")}
+                                className={"rounded-full hover:bg-manuka ml-4"}>Next</Button>
+                        {/*Add this submit button to final form*/}
+                        {/*<PrimaryButton className="ms-4" disabled={processing}>*/}
+                        {/*    Register*/}
+                        {/*</PrimaryButton>*/}
+                    </div>
+                </> : content == "Platform" ?
+                <>
+                    <InputLabel htmlFor="platforms" value="Which platforms do you create content for?"/>
+                    <ToggleGroup
+                        variant={"outline"}
+                        type="multiple"
+                        className={"mt-4"}
+                        onValueChange={(value: any) => {
+                            if (value) setData('platforms', value);
+                        }}
                     >
-                        Already registered?
-                    </Link>
+                        <ToggleGroupItem
+                            className={"font-bold radix-state-on:bg-red-600 radix-state-on:text-white"}
+                            value="youtube"
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
+                        >
+                            YouTube
+                        </ToggleGroupItem>
+                        <ToggleGroupItem
+                            className={"font-bold radix-state-on:bg-pink-600 radix-state-on:text-white"}
+                            value="instagram"
+                        >
+                            Instagram
+                        </ToggleGroupItem>
+                        <ToggleGroupItem
+                            className={"font-bold radix-state-on:bg-purple-600 radix-state-on:text-white"}
+                            value="twitch"
+                        >
+                            Twitch
+                        </ToggleGroupItem>
+                        <ToggleGroupItem
+                            className={"font-bold radix-state-on:bg-foreground radix-state-on:text-background"}
+                            value="tiktok"
+                        >
+                            TikTok
+                        </ToggleGroupItem>
+                        <ToggleGroupItem
+                            className={"font-bold radix-state-on:bg-blue-700 radix-state-on:text-white"}
+                            value="linkedin"
+                        >
+                            LinkedIn
+                        </ToggleGroupItem>
+                    </ToggleGroup>
+                </> : <p>Personal</p>
+            }
             </form>
         </GuestLayout>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -69,5 +69,9 @@ export default {
     	}
     },
 
-    plugins: [forms, require("tailwindcss-animate")],
+    plugins: [
+        forms,
+        require("tailwindcss-animate"),
+        require("tailwindcss-radix")()
+    ],
 };
