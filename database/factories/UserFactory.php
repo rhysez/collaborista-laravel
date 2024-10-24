5643f9ad7578c697a2cb873e0f97d7b04cf32ee6
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-10-24 15:03:54 +0100

    store user.platforms values as Platform foreign key instead of a string

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Platform;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -30,7 +31,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'bio' => fake()->text(),
-            'platforms' => ['youtube', 'instagram'],
+            'platforms' => [],
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Platform;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,12 +14,15 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $platform = Platform::where('name', 'youtube')->first();
+
         // Our test user
         User::factory()->create([
             'name' => 'Test User',
             'username' => 'testuser',
             'email' => 'test@example.com',
             'bio' => 'Hello! I am a test user for Collaborista',
+            'platforms' => [$platform->id]
         ]);
 
         // Additional test users
